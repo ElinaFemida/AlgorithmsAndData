ec23cc47b8b2ad874263f3698506cccf173f1908
Elina <ya.ilina4@yandex.ru>
2021-07-27 09:30:25 +0300

    lesson 6 changed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,24 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
+        Random random = new Random();
 
+        int balancedTrees = 0;
+        int trees = 2000000;
+
+        for (int i = 0; i < trees; i++) {
+            TreeMap<Integer, Integer> map = new TreeMap<>();
+            int value = 0;
+            while (map.height() < 6) {
+                value = random.nextInt(201) - 100;
+                map.put(value, value);
+            }
+            map.delete(value);
+            if (map.isBalanced())
+                balancedTrees++;
+        }
+        System.out.println("Процент сбалансированных деревьев = " + (double) balancedTrees / trees * 100 + "% :");
+        System.out.println(balancedTrees + " из " + trees);
     }
 }
